9161c1e4be9b3f53a314d56c692a792eca4af340
Kimble Bronson <133877479+bronski81367@users.noreply.github.com>
2025-06-12 12:28:34 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to TeknoGeek.com.")
+name = input("What is your name? ")
+print("Hello " + name)
+birth_year = input("Enter your birth year: ")
+age = 2025 - int(birth_year)
+print(age)
